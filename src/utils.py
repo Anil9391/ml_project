b3e8e4c4b1a0c3b2a77ce8178e691c2bafd9a657
unknown <singalanil1995@gmail.com>
2025-10-08 20:18:06 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import dill
+import pickle
 
 import pandas as pd
 import numpy as np
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -18,5 +20,18 @@ def save_object(file_path, obj):
             
     except Exception as e:
         raise CustomException(e,sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    from sklearn.metrics import r2_score, mean_squared_error
+
+    model_report = {}
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        score = r2_score(y_test, y_pred)
+        model_report[name] = score
+        
+    return model_report
+
 
 
