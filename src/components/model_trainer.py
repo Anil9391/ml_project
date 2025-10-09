6814a3881485649461deaebb88baeb4bf14332f8
unknown <singalanil1995@gmail.com>
2025-10-09 19:55:27 +0530

    End to end Project using Flask

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -48,6 +48,7 @@ class ModelTrainer:
                 "AdaBoost Classifier" : AdaBoostRegressor(),
             }
             
+            
             #model_report:dict = evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
             model_report = evaluate_model(X_train, y_train, X_test, y_test, models)
 
--- src/utils.py
+++ src/utils.py
@@ -33,5 +33,16 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
         
     return model_report
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise Exception(f"Error loading object: {e}")
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+        
+
 
 
